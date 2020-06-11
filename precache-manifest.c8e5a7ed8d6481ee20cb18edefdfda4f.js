eb57efa6d93d47802a8de285c82ddc2b6eb320c3
AKolte <adi.kolte@gmail.com>
2020-06-11 14:01:08 +0530

    Updates

---
+++ precache-manifest.c8e5a7ed8d6481ee20cb18edefdfda4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5284e25395d262e6fcf7686b09d6c10",
+    "url": "/ReactTodoList/index.html"
+  },
+  {
+    "revision": "ddbcb9fb04c9c5918532",
+    "url": "/ReactTodoList/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "21ce1591714df13a3a91",
+    "url": "/ReactTodoList/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ddbcb9fb04c9c5918532",
+    "url": "/ReactTodoList/static/js/2.82d152b1.chunk.js"
+  },
+  {
+    "revision": "246ad901f08703825af5eeab9ed7a0eb",
+    "url": "/ReactTodoList/static/js/2.82d152b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ce1591714df13a3a91",
+    "url": "/ReactTodoList/static/js/main.2ba1308e.chunk.js"
+  },
+  {
+    "revision": "c3e9ec3062af248f8c82",
+    "url": "/ReactTodoList/static/js/runtime-main.db505bed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactTodoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
